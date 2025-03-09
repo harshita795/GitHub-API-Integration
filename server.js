d59893637901ada14e63bacb4928da0c7242c081
harshita795 <yadavharshita261303@gmail.com>
2025-03-09 11:42:55 +0530

    feat: added root route

--- server.js
+++ server.js
@@ -9,5 +9,18 @@ app.use(cors());
 
 app.use("/github", githubRoutes);
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the GitHub API Integration!",
+    available_routes: {
+      "GET /github":
+        "Fetch GitHub profile details (followers, following, repositories).",
+      "GET /github/{repo-name}": "Fetch details of a specific repository.",
+      "POST /github/{repo-name}/issues":
+        "Create a new issue in the specified repository.",
+    },
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
